0993feef7eb45618018881699035821d4f07e218
John Kim <60197073+dudghks@users.noreply.github.com>
2023-02-28 10:18:39 -0500

    rotate
    it works

--- main.py
+++ main.py
@@ -5,29 +5,29 @@ screen = pygame.display.set_mode((1366, 768))
 
 # 0: I, 1: J, 2: L, 3: O, 4: S, 5: T, 6: Z
 START_PIECES = {
-   0:[['_', '_', '_', '_', '_'],
-      ['_', '_', '_', '_', '_'],
-      ['_', 'I', 'I', 'I', 'I'],
-      ['_', '_', '_', '_', '_'],
-      ['_', '_', '_', '_', '_']],
-   1:[['J', '_', '_'],
+   0:[['-', '-', '-', '-', '-'],
+      ['-', '-', '-', '-', '-'],
+      ['-', 'I', 'I', 'I', 'I'],
+      ['-', '-', '-', '-', '-'],
+      ['-', '-', '-', '-', '-']],
+   1:[['J', '-', '-'],
       ['J', 'J', 'J'],
-      ['_', '_', '_']],
-   2:[['_', '_', 'L'],
+      ['-', '-', '-']],
+   2:[['-', '-', 'L'],
       ['L', 'L', 'L'],
-      ['_', '_', '_']],
-   3:[['_', 'O', 'O'],
-      ['_', 'O', 'O'],
-      ['_', '_', '_']],
-   4:[['_', 'S', 'S'],
-      ['S', 'S', '_'],
-      ['_', '_', '_']],
-   5:[['_', 'T', '_'],
+      ['-', '-', '-']],
+   3:[['-', 'O', 'O'],
+      ['-', 'O', 'O'],
+      ['-', '-', '-']],
+   4:[['-', 'S', 'S'],
+      ['S', 'S', '-'],
+      ['-', '-', '-']],
+   5:[['-', 'T', '-'],
       ['T', 'T', 'T'],
-      ['_', '_', '_']],
-   6:[['Z', 'Z', '_'],
-      ['_', 'Z', 'Z'],
-      ['_', '_', '_']]
+      ['-', '-', '-']],
+   6:[['Z', 'Z', '-'],
+      ['-', 'Z', 'Z'],
+      ['-', '-', '-']]
 }
 
 COLORS = {
@@ -43,18 +43,18 @@ COLORS = {
 
 # 0: I | 1: J, L, S, T, Z | 2: O
 OFFSET_BOOK = {
-   0:(((0, 0), (-1, 0), (2, 0), (-1, 0), (2, 0)),
-      ((-1, 0), (0, 0), (0, 0), (0, -1), (0, 2)),
-      ((-1, -1), (1, -1), (-2, -1), (1, 0), (-2, 0)),
-      ((0, -1), (0, -1), (0, -1), (0, 1), (0, -2))),
+   0:(((0, 0), (1, 0), (-2, 0), (1, 0), (-2, 0)),
+      ((1, 0), (0, 0), (0, 0), (0, 1), (0, -2)),
+      ((1, 1), (-1, 1), (2, 1), (-1, 0), (2, 0)),
+      ((0, 1), (0, 1), (0, 1), (0, -1), (0, 2))),
    1:(((0, 0), (0, 0), (0, 0), (0, 0), (0, 0)),
-      ((0, 0), (1, 0), (1, 1), (0, -2), (1, -2)),
+      ((0, 0), (-1, 0), (-1, -1), (0, 2), (-1, 2)),
       ((0, 0), (0, 0), (0, 0), (0, 0), (0, 0)),
-      ((0, 0), (-1, 0), (-1, 1), (0, -2), (-1, -2))),
+      ((0, 0), (1, 0), (1, -1), (0, 2), (1, 2))),
    2:(((0, 0),),
-      ((0, 1),),
-      ((-1, 1),),
-      ((-1, 0),))
+      ((0, -1),),
+      ((1, -1),),
+      ((1, 0),))
 }
 class Piece:
    piece_type = 0
@@ -76,9 +76,8 @@ class Piece:
          input_piece = self.shape
       for i in range(len(input_grid)):
          for j in range(len(input_grid[i])):
-            # if a filled block in the shape array (not '_')
-            # is in the same location as one in the input grid
-            if input_piece[i][j] != '_' and input_grid[i][j] != '_':
+            # if both test blocks are filled, there is a collision
+            if input_piece[i][j] != '-' and input_grid[i][j] != '_':
                return True
       # If no collision is found, return false
       return False
@@ -100,10 +99,10 @@ class Field:
       self.offset_amt = (0, 0)
       if current_piece == 0:
          self.piece_x = 2
-         self.piece_y = -2
+         self.piece_y = 0
       else:
          self.piece_x = 3
-         self.piece_y = 0
+         self.piece_y = 2
       self.piece_rot = 0
    
    def find_area_of_interest(self, x, y, width, height):
@@ -154,23 +153,25 @@ class Field:
       # print(kick_translations)
       # print('========================================')
       # Test kick translations for collision with filled blocks
-      for idx, offsets in enumerate(kick_translations):
+      for offsets in kick_translations:
          x_offset, y_offset = offsets
          # Find area of interest for collision checking
-         new_spot = self.find_area_of_interest(self.piece_x + x_offset + self.offset_amt[0], self.piece_y + y_offset + self.offset_amt[1], len(self.current_piece.shape), len(self.current_piece.shape))
+         new_spot = self.find_area_of_interest(self.piece_x + x_offset, self.piece_y + y_offset, len(self.current_piece.shape), len(self.current_piece.shape))
          # for i in new_spot:
          #    print(i)
+         print(new_spot)
          outcome = self.current_piece.check_collision(new_spot, rotated_piece)
          print(outcome)
          if not outcome:
+            self.piece_x += x_offset
+            self.piece_y  += y_offset
             self.piece_rot = new_rot
-            self.offset_amt = OFFSET_BOOK.get(self.current_piece.offset)[new_rot][idx]
             print(self.offset_amt)
             self.current_piece.shape = rotated_piece
             break
    
    def move(self, dir_x, dir_y):
-      new_spot = self.find_area_of_interest(self.piece_x + dir_x + self.offset_amt[0], self.piece_y + dir_y + self.offset_amt[1], len(self.current_piece.shape), len(self.current_piece.shape))
+      new_spot = self.find_area_of_interest(self.piece_x + dir_x, self.piece_y + dir_y, len(self.current_piece.shape), len(self.current_piece.shape))
       collision = self.current_piece.check_collision(new_spot)
       if not collision:
          self.piece_x += dir_x
@@ -180,11 +181,12 @@ class Field:
    def display(self, x_offset, y_offset):
       for y, i in enumerate(self.game_grid):
          for x, j in enumerate(i):
-            pass
-            # pygame.draw.rect(screen, COLORS.get(j), (x * 25 + x_offset, y * 25 + y_offset, 25, 25))
+            # pass
+            pygame.draw.rect(screen, COLORS.get(j), (x * 25 + x_offset, y * 25 + y_offset, 25, 25))
       for y, i in enumerate(self.current_piece.shape):
          for x, j in enumerate(i):
-            pygame.draw.rect(screen, COLORS.get(j), ((x + self.piece_x + self.offset_amt[0]) * 25 + x_offset, (y + self.piece_y + self.offset_amt[1]) * 25 + y_offset, 25, 25))
+            if j != '-':
+               pygame.draw.rect(screen, COLORS.get(j), ((x + self.piece_x) * 25 + x_offset, (y + self.piece_y) * 25 + y_offset, 25, 25))
             
 
    
@@ -193,8 +195,30 @@ class Field:
 running = True
 background = pygame.transform.scale(pygame.image.load('background.jpg'), (1600, 900))
 
-test_field = Field(3)
-
+test_field = Field(2)
+test_field.game_grid = [
+['_', '_', '_', '_', '_', '_', '_', '_', '_', '_'],
+['_', '_', '_', '_', '_', '_', '_', '_', '_', '_'],
+['_', '_', '_', '_', '_', '_', '_', '_', '_', '_'],
+['_', '_', '_', '_', '_', '_', '_', '_', '_', '_'],
+['_', '_', '_', '_', '_', '_', '_', '_', '_', '_'],
+['_', '_', '_', '_', '_', '_', '_', '_', '_', '_'],
+['_', '_', '_', '_', '_', '_', '_', '_', '_', '_'],
+['_', '_', '_', '_', '_', '_', '_', '_', '_', '_'],
+['_', '_', '_', '_', '_', '_', '_', '_', '_', '_'],
+['_', '_', '_', '_', '_', '_', '_', '_', '_', '_'],
+['_', '_', '_', '_', '_', '_', '_', '_', '_', '_'],
+['_', '_', '_', '_', '_', '_', '_', '_', '_', '_'],
+['_', '_', '_', '_', '_', '_', '_', '_', '_', '_'],
+['_', '_', '_', '_', '_', '_', '_', '_', '_', '_'],
+['_', '_', '_', '_', '_', '_', '_', '_', '_', '_'],
+['_', '_', '_', '_', '_', '_', '_', '_', '_', '_'],
+['_', '_', '_', '_', '_', '_', '_', '_', '_', '_'],
+['_', '_', '_', '_', '_', '_', '_', '_', '_', '_'],
+['T', '_', '_', 'T', '_', '_', '_', '_', '_', '_'],
+['T', '_', '_', 'T', '_', '_', '_', '_', '_', '_'],
+['_', '_', '_', 'T', '_', '_', '_', '_', '_', '_'],
+['_', 'T', 'T', 'T', '_', '_', '_', '_', '_', '_']]
 while running:
    # Background
    screen.fill((0, 0, 0))
@@ -211,6 +235,10 @@ while running:
                test_field.move(1, 0)
          if event.key == pygame.K_LEFT:
                test_field.move(-1, 0)
+         if event.key == pygame.K_DOWN:
+               test_field.move(0, 1)
+         if event.key == pygame.K_UP:
+               test_field.move(0, -1)
          if event.key == pygame.K_z:
                test_field.rotate(-1)
          if event.key == pygame.K_x:
